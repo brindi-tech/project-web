7e5d207c26e7d6e0c1481a0d6f120324808865c3
brindi-tech <brindi.ofc@gmail.com>
2026-09-18 00:22:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,32 +3,32 @@ const coin = document.getElementById("coin");
 const profileName = document.getElementById("profile-name");
 const profileDescription = document.getElementById("profile-description");
 
-let showingMe = true;
+let showingPerson1 = true;
 let rotation = 0;
 
 
 coin.addEventListener("click", function () {
 
-    showingMe = !showingMe;
+    showingPerson1 = !showingPerson1;
 
     rotation += 180;
 
     coin.style.transform = `rotateY(${rotation}deg)`;
 
 
-    if (showingMe) {
+    if (showingPerson1) {
 
-        profileName.textContent = "O MEU NOME";
+        profileName.textContent = "NOME DO COLEGA 1";
 
         profileDescription.textContent =
-            "Aqui aparece toda a informação sobre mim.";
+            "Aqui aparece toda a informação sobre a Pessoa 1.";
 
     } else {
 
-        profileName.textContent = "NOME DO COLEGA";
+        profileName.textContent = "NOME DO COLEGA 2";
 
         profileDescription.textContent =
-            "Aqui aparece toda a informação sobre o meu colega.";
+            "Aqui aparece toda a informação sobre a Pessoa 2.";
 
     }
 
